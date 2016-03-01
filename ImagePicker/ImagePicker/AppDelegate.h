3f1e343546bbbc0890d02031c6b2b1fa71532484
Robyn Van Deventer <robynv@gridstone.com.au>
2016-03-02 10:44:52 +1100

    Adding project files

---
+++ ImagePicker/ImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePicker
+//
+//  Created by Robyn Van Deventer on 2/03/2016.
+//  Copyright © 2016 Robyn Van Deventer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
